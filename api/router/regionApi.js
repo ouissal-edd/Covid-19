d0d2168edfc5507086904e3fb23a141357582e00
ouissal-edd <ouissal.edouj21@gmail.com>
2022-03-14 21:38:28 +0100

    v3

--- api/router/regionApi.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {createRegion,getAllRegion}= require('../controllers/regionController');
-
-router.post('/',createRegion);
-router.get('/',getAllRegion);
-
-
-module.exports = router;
